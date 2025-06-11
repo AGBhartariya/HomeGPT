46bba5b6ec8eb0daa21ac14a0d9220a606d0373c
Abhigyan Bhartariya <abhigyangb27@gmail.com>
2025-06-11 17:34:13 +0530

    Add gpt_chat.py

--- utils/gpt_chat.py
+++ utils/gpt_chat.py
@@ -1,16 +1,17 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+import streamlit as st
 
 # Load environment variables from .env file
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-
-genai.configure(api_key=api_key)
+# load_dotenv()
+# api_key = os.getenv("GEMINI_API_KEY")
 
 def ask_homegpt(prompt):
+    api_key = st.secrets["GEMINI_API_KEY"]
+    genai.configure(api_key=api_key)
     try:
-        # Choose the model (e.g., "gemini-1.5-pro-latest")
+        
         model = genai.GenerativeModel("gemini-2.0-flash")
         response = model.generate_content(
             f"You are a very warm, friendly family assistant named HomeGPT who provides solutions to every question in a very respectful manner and cares for people. Make it in the best way possible.\n\nUser: {prompt}"
