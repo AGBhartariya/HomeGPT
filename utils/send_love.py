5fdc53c3b9902f938e97037a09ca75b97ff0ed7d
Abhigyan Bhartariya <abhigyangb27@gmail.com>
2025-06-12 23:27:32 +0530

    Add send_love.py

--- utils/send_love.py
+++ utils/send_love.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import requests
-def send_telegram_message(user_message):
+def send_telegram_message(user_message,sender_name):
     # Replace with your bot token and chat_id (your Telegram user ID)
     bot_token = st.secrets["TELEGRAM_BOT_TOKEN"]  # Store in Streamlit secrets for security
     chat_id = st.secrets["TELEGRAM_CHAT_ID"]
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    text = f"📩 Message from {sender_name}:\n{user_message}"
     data = {
         "chat_id": chat_id,
         "text": user_message
